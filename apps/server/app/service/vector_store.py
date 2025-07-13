a580a9a2793a527b2fd9ab7f52972867de4abc24
jin-Pro <dnjun2@ajou.ac.kr>
2025-07-14 00:44:36 +0900

    enhance: VectorStore delete handler

--- apps/server/app/service/vector_store.py
+++ apps/server/app/service/vector_store.py
@@ -1,34 +1,36 @@
-import chromadb
-from chromadb.config import Settings
+from chromadb import PersistentClient  # 🔹 요거 필요
+import uuid
 
-# persist_directory만 지정 (chroma_db_impl 옵션 제거)
-settings = Settings(persist_directory="./vectorstore")
-
-_client = chromadb.Client(settings=settings)
+_client = PersistentClient(path="/app/vectorstore")
 _collection = _client.get_or_create_collection(name="docs")
-
-
-
-# from chromadb.config import Settings
-# import chromadb
-
-# settings = Settings(
-#     persist_directory="./vectorstore"
-# )
-
-# client = chromadb.Client(settings=settings)
 
 def get_vector_db():
     return _collection
 
-def save_to_vector_db(chunks, embeddings, collection):
+def save_to_vector_db(chunks, embeddings,filename):
     for i, (chunk, embedding) in enumerate(zip(chunks, embeddings)):
-        collection.add(
-            ids=[f"doc_{i}"],
-            embeddings=[embedding],
-            documents=[chunk]
-        )
+        try:
+            unique_id = str(uuid.uuid4())  # 항상 고유한 ID 생성
+            _collection.add(
+                ids=[f"doc_{filename}_{i}_{unique_id}"],
+                embeddings=[embedding],
+                documents=[chunk]
+            )
+            print(f"Added chunk {i}")
+        except Exception as e:
+            print(f"Error adding chunk {i}: {e}")
 
-def query_similar_chunks(query_embedding, collection, k: int = 5):
-    results = collection.query(query_embeddings=[query_embedding], n_results=k)
+def query_similar_chunks(query_embedding, k: int = 5):
+    results = _collection.query(query_embeddings=[query_embedding], n_results=k)
     return results['documents'][0]
+
+def delete(target:str):
+    results = _collection.get()
+
+    # 관련 ID만 필터링
+    related_ids = [doc_id for doc_id in results['ids'] if doc_id.startswith(target)]
+
+    if related_ids:
+        _collection.delete(ids=related_ids)
+    
+    return True
