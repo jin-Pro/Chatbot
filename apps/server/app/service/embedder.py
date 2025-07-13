4e0af24225ae2ef351e159be73ea81f7e2ea9476
jin-Pro <dnjun2@ajou.ac.kr>
2025-07-14 00:45:34 +0900

    fix: embed - openai => sentence_transformers

--- apps/server/app/service/embedder.py
+++ apps/server/app/service/embedder.py
@@ -1,9 +1,18 @@
-from openai import OpenAI
-openai_client = OpenAI(api_key="your-openai-api-key")
+# from openai import OpenAI
+
+# openai_client = OpenAI(api_key="your-openai-api-key")
+
+# def get_embedding(text: str) -> list[float]:
+#     response = openai_client.embeddings.create(
+#         input=[text],  # 리스트 형태로 보내는 것이 권장
+#         model="text-embedding-3-small"
+#     )
+#     return response.data[0].embedding
+
+
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def get_embedding(text: str):
-    response = openai_client.embeddings.create(
-        input=text,
-        model="text-embedding-3-small"
-    )
-    return response.data[0].embedding
+    return model.encode(text).tolist()
